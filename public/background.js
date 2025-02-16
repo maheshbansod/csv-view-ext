71f3ddefe2aac2052484e0e097d54b380fca51e8
Mahesh Bansod <coolkevin011@gmail.com>
2025-02-16 12:52:26 +0530

    make ext redirect

---
+++ public/background.js
@@ -0,0 +1,17 @@
+
+const EXT_PAGE = chrome.runtime.getURL('/index.html');
+const RULES = [{
+  id: 1,
+  action: {
+    type: 'redirect',
+    redirect: { regexSubstitution: EXT_PAGE + '#\\0' },
+  },
+  condition: {
+    regexFilter: '^.+.csv$',
+    resourceTypes: ['main_frame', 'sub_frame'],
+  },
+}];
+chrome.declarativeNetRequest.updateDynamicRules({
+  removeRuleIds: RULES.map(r => r.id),
+  addRules: RULES,
+});
