75bc27a43ab5602123b1102b9b9ba915cf3e58b6
Mahesh Bansod <coolkevin011@gmail.com>
2025-02-16 17:20:08 +0530

    support firefox

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import manifestJson from './manifest.json';
+import fs from 'fs';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ]
-})
+export default defineConfig(({ mode }) => {
+    const OUT_DIR = 'dist';
+    return {
+        plugins: [
+            vue(),
+            tailwindcss(),
+            {
+                name: 'copy-manifest',
+                closeBundle() {
+                    console.info("Copying manifest file");
+                    const finalManifest = mode === 'firefox' ? {
+                        ...manifestJson,
+                        background: {
+                            scripts: [manifestJson.background.service_worker],
+                        }
+                    } : manifestJson;
+                    fs.writeFile(`${OUT_DIR}/manifest.json`, JSON.stringify(finalManifest), (err) => {
+                        if (err) {
+                            console.error("Couldn't write the manifest file");
+                            console.error(err);
+                            return;
+                        }
+                        console.info("Manifest file written");
+                    });
+                },
+            }
+        ]
+    }
+});
