12f4bbe1d5efcd94df77ae7540e0c3587c9bb66a
Mahesh Bansod <coolkevin011@gmail.com>
2025-02-20 19:13:51 +0530

    add chrome ext url

--- src/components/CsvUrlInput.vue
+++ src/components/CsvUrlInput.vue
@@ -207,7 +207,7 @@ const handleSubmit = () => {
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
           </svg>
-          Install for Chrome ⚠️ WIP
+          Install for Chrome
         </a>
         <a
           :href="firefoxExtensionUrl"
--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 
-export const chromeExtensionUrl = 'https://github.com/maheshbansod/csv-view-ext';
+export const chromeExtensionUrl = 'https://chromewebstore.google.com/detail/csv-viewer/nkkliddllkckohhkodjcbajhnbaljlag';
 export const firefoxExtensionUrl = 'https://addons.mozilla.org/en-US/firefox/addon/csv-viewer/';
 export const extensionUrl = 'https://github.com/maheshbansod/csv-view-ext';
