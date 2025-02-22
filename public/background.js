773993432e5ed5d3c3857b7843198c6cad459fe7
Mahesh Bansod <coolkevin011@gmail.com>
2025-02-22 22:18:30 +0530

    fix: file should end in dot csv

--- public/background.js
+++ public/background.js
@@ -1,17 +1,17 @@
-
-const EXT_PAGE = chrome.runtime.getURL('/index.html');
+const EXT_PAGE = chrome.runtime.getURL("/index.html");
 const RULES = [{
-  id: 1,
-  action: {
-    type: 'redirect',
-    redirect: { regexSubstitution: EXT_PAGE + '#\\0' },
-  },
-  condition: {
-    regexFilter: '^.+.csv$',
-    resourceTypes: ['main_frame', 'sub_frame'],
-  },
+    id: 1,
+    action: {
+        type: "redirect",
+        redirect: { regexSubstitution: EXT_PAGE + "#\\0" },
+    },
+    condition: {
+        regexFilter: "^.+\.csv$",
+        resourceTypes: ["main_frame", "sub_frame"],
+    },
 }];
 chrome.declarativeNetRequest.updateDynamicRules({
-  removeRuleIds: RULES.map(r => r.id),
-  addRules: RULES,
+    removeRuleIds: RULES.map((r) => r.id),
+    addRules: RULES,
 });
+
